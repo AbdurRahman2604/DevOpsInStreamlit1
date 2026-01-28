d9bec14068395f5e3c1bd5bd128664181de46f94
AbdurRahman2604 <arr2604@gmail.com>
2026-01-28 11:28:59 +0530

    modified 2

--- AdditionApp.py
+++ AdditionApp.py
@@ -2,8 +2,9 @@ import streamlit as st
 a = 10
 b = 20
 c=10
-result = a * b * c
+d=10
+result = a * b * c *d
 # Streamlit app
 st.title("Multiplication App")
 st.text("Welcome")
-st.write(f"The sum of {a} ,{b} {c} is: {result}")
+st.write(f"The sum of {a} ,{b} {c} {d} is: {result}")
